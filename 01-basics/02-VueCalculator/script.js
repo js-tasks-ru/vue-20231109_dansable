83daee80d1a12196d8524d50efdb7ded7eab23a1
Daniil Sobolev <lnook90l@gmail.com>
2023-11-23 14:58:57 +0200

    implement vue calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,41 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp} from './vendor/vue.esm-browser.js';
+import {defineComponent} from "./vendor/vue.esm-browser";
+
+const operatorsMap = {
+  sum: 'sum',
+  subtract: 'subtract',
+  multiply: 'multiply',
+  divide: 'divide'
+};
 
 // Создайте Vue приложение
+const App = defineComponent({
+  operatorsMap: operatorsMap,
+
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      operator: ''
+    }
+  },
+
+  computed: {
+    sum() {
+      switch (this.operator) {
+        case operatorsMap.sum:
+          return this.firstNumber + this.secondNumber;
+        case operatorsMap.subtract:
+          return this.firstNumber - this.secondNumber;
+        case operatorsMap.multiply:
+          return this.firstNumber * this.secondNumber;
+        case operatorsMap.divide:
+          return this.firstNumber / this.secondNumber;
+        default:
+          return 0;
+      }
+    }
+  }
+});
+
+createApp(App).mount('#app');
