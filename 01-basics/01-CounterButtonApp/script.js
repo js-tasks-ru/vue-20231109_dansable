3ef2b5662ef00fe217a5fc53f2a6174359f9216f
Daniil Sobolev <lnook90l@gmail.com>
2023-11-23 14:38:57 +0200

    implement counter button app

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,18 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  data() {
+    return {
+      count: 0
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    }
+  },
+});
+
+createApp(App).mount('#app');
