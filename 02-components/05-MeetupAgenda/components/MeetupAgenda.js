7d5fb47160472f4371185651e5169af9a2d78b69
dansable <dan.sable@yandex.ru>
2023-11-25 16:23:02 +0200

    implement meetup agenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,26 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import {defineComponent} from '../vendor/vue.esm-browser.js';
+import MeetupAgendaItem from "./MeetupAgendaItem";
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  props: {
+    agenda: {
+      type: Array,
+      required: true
+    }
+  },
+
+  components: {
+    MeetupAgendaItem
+  },
+
   template: `
     <ul class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <MeetupAgendaItem v-for="agendaItem in agenda"
+                          :key="agendaItem.id"
+                          :agendaItem="agendaItem"/>
       </li>
     </ul>`,
 });
